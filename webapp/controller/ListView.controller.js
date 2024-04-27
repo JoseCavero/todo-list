7937e90fe0f74c24235b0e557334a3afb5f9d960
Jose Antonio Cavero <joseantoniocavero15@gmail.com>
2024-04-27 16:00:51 +0200

    Setting logic for adding new task

--- webapp/controller/ListView.controller.js
+++ webapp/controller/ListView.controller.js
@@ -19,6 +19,25 @@ sap.ui.define([
                     ID: oCtxt.getProperty("ID")
                 });
 
+            },
+            onAddTodo: function (oEvent) {
+                // Example to recorver value from oEvent
+               var sTitle = oEvent.getParameter("value");
+
+               // NO nee to do it because we binded it to model
+               // Get the data model
+               var oData = this.getView().getModel().getData();
+               // Build the new task item
+               var oNewTask = {
+                    ID: oData.todos.length,
+                    title: "Tarea de química",
+                    category: "Lengua",
+                    description: "Ejercicios de sintaxis",
+                    completed: false
+               }
+               oData.todos.push(oNewTask);
+               oData.newEntry = "";
+               this.getView().getModel().setData(oData);
             }
         });
     });
